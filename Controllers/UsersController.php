ede8732c5e0164579ed567d920c694feb0f06709
nbahire <n.bahire31@gmail.com>
2020-08-28 21:36:55 +0200

    reglage du controller de users

--- Controllers/UsersController.php
+++ Controllers/UsersController.php
@@ -10,27 +10,30 @@ class UsersController extends Controller
     public function login()
     {
         // On vérifie si notre post contient les champs email et password
-        
+
         if (!empty($_POST['email']) && !empty($_POST['password'])) {
             // On nettoie l'e-mail et on chiffre le mot de passe
             $userModel = new UsersModel;
-            $userArray = $userModel->findByEmail(strip_tags($_POST['email']));           
+            $userArray = $userModel->findByEmail(strip_tags($_POST['email']));
             if (!$userArray) {
-                http_response_code(404);
-                $_SESSION['erreur'] = "Votre mot de passe est incorecte!!";
+
+                $_SESSION['erreur'] = " Identifiants incorectes !! ";
                 header('Location: login');
                 exit;
             }
             $user = $userModel->hydrate($userArray);
-            
+
             if (password_verify($_POST['password'], $user->getPassword())) {
-                $user->setSession(); 
-                
+                $user->setSession();
+                if (isset($_SESSION['user']['roles']) && in_array('ROLE_ADMIN', $_SESSION['user']['roles'])) {
+                    header('Location: /admin');
+                }
+            } else {
+                $_SESSION['erreur'] = " Identifiants incorectes !! ";
+                header('Location: login');
+                exit;
             }
             $this->render('users/login', compact('user'));
-            if (isset($_SESSION['user']['roles']) && in_array('ROLE_ADMIN', $_SESSION['user']['roles'])){
-                header('Location: /PROJET4/public/admin'); 
-            }
         }
         $this->render('users/login');
     }
@@ -41,7 +44,7 @@ class UsersController extends Controller
     public function logout()
     {
         unset($_SESSION['user']);
-        header('Location: ' . $_SERVER['HTTP_REFERER']);
+        header('Location: /main');
         exit;
     }
 }
